e5ac1236e6ded13550f21180da0ab3dee2ae7b9c
znova.roman <roman.znova@omnigon.com>
2019-02-03 18:09:29 +0200

    move-to-git

--- android/app/src/main/java/com/ubilling/MainApplication.java
+++ android/app/src/main/java/com/ubilling/MainApplication.java
@@ -3,6 +3,7 @@ package com.ubilling;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.psykar.cookiemanager.CookieManagerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new VectorIconsPackage(),
             new RNSoundPackage(),
             new CookieManagerPackage()
--- src/actions/search.js
+++
@@ -1,8 +0,0 @@
-import createAction from 'redux-actions/es/createAction';
-
-export const REQUEST_SEARCH = 'REQUEST_SEARCH';
-export const RESPONSE_SEARCH = 'RESPONSE_SEARCH';
-export const SET_USERS_SEARCH_RESULT = 'SET_USERS_SEARCH_RESULT';
-export const SET_TASKS_SEARCH_RESULT = 'SET_TASKS_SEARCH_RESULTS';
-
-// export const setTaskByDate = createAction(SET_TASK_BY_DATE, payload => payload);
