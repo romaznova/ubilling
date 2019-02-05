a8f80d7fe5f7e454611fce9ad6d532d93618ab7f
znova.roman <roman.znova@omnigon.com>
2019-02-05 12:30:16 +0200

    update app logo for default

--- src/containers/Logo/index.js
+++ src/containers/Logo/index.js
@@ -4,8 +4,8 @@ import React from 'react';
 export const Logo = () => (
     <View style={{flexDirection: 'row', justifyContent: 'center', marginBottom: 10}}>
         <Image resizeMode='contain'
-            source={require('../../images/Ubilling_logo.png')}
-            style={{width: 240, height: 150}}
+            source={require('../../images/header-img-unicorn.png')}
+            style={{width: 240, height: 160}}
         />
     </View>
 );
